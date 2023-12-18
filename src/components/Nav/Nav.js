cda8b21967cabee217bd795f3f0a86ccbfeec5e6
Sally Do <56125850+sallydough@users.noreply.github.com>
2023-12-17 18:46:42 -0700

    change nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,11 +1,42 @@
 import React from 'react'
+import Slider from "react-slick";
 import './nav.css'
 
 const Nav = () => {
+
+  const settings = {
+    className: "center",
+    centerMode: true,
+    infinite: true,
+    centerPadding: "60px",
+    slidesToShow: 5,
+    speed: 500
+  };
+
   return (
-    <section id="nav">
-      Nav
-    </section>
+    
+    <div className="carousel-1">
+      <Slider {...settings}>
+      <div className="box">
+        <h3>1</h3>
+      </div>
+      <div className="box">
+        <h3>2</h3>
+      </div>
+      <div className="box">
+        <h3>3</h3>
+      </div>
+      <div className="box">
+        <h3>4</h3>
+      </div>
+      <div className="box">
+        <h3>5</h3>
+      </div>
+      <div className="box">
+        <h3>6</h3>
+      </div>
+      </Slider>
+    </div>
   )
 }
 
